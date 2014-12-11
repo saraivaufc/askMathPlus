2f3a7afe03a345e520bae9965bcb9e51da2d4f23
saraivaufc <saraiva.ufc@gmail.com>
2014-12-11 05:55:12 -0300

    melhorando os avisos

--- matbasica/templates/cadastro/criarContaSucesso.php
+++ matbasica/templates/cadastro/criarContaSucesso.php
@@ -3,6 +3,6 @@
 {% block conteudo %}
 	<center>
 		<h1>Usuario Criado Com Sucesso!!!</h1>
-		<button class="botao caixa botao-1" onClick="document.location='/login/' ">Fazer Login</button>
+		<button class="btn botao caixa botao-1" onClick="document.location='/login/' ">Fazer Login</button>
 	</center>
 {% endblock %}
