84f78b894c91c1e64b15dacaf7443a05d39edb04
saraivaufc <saraiva.ufc@gmail.com>
2014-12-17 20:35:25 -0300

    iniciando o forum

--- sisport/urls.py
+++ sisport/urls.py
@@ -32,7 +32,7 @@ urlpatterns = patterns('',
 
     url(r'^is_logado/$', is_logado),
     
-    url(r'^forum/$', include('forums.urls', namespace='forums')),
+    url(r'^forum/', include('forums.urls', namespace='forums')),
 
     url(r'^admin/', include(admin.site.urls)),
 )	
