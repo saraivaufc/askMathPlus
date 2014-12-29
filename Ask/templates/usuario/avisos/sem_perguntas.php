c94f899fbafbfd6c07328d714961cd82ecc6062f
saraivaufc <saraiva.ufc@gmail.com>
2014-12-29 10:37:37 -0300

    add sem perguntas

--- Ask/templates/usuario/avisos/sem_perguntas.php
+++ Ask/templates/usuario/avisos/sem_perguntas.php
@@ -1,33 +1,22 @@
-{% extends 'usuario/secundario/secundario.php' %}
+{% extends 'usuario/secundario/secundarioOpcoes.php' %}
+
+{% block titulo-inicial  %}
+	<h3>Licao Nao Possui Perguntas</h3>
+{% endblock %}
+
+{% block table-pontuacao-all %}
+
+{% endblock %}
 
 
-{%  block funcoes %}
-	$(document).ready(function(){
-		$(".jspPane").css("height","100%");
-	});
-{%  endblock %}
-
-{% block conteudo-right %}
-	<div class="conteudo-right bg-info">
-		<div class="font-dconteudo">
-			<div class="descricao-pergunta">
-				<div class="avisos-conteudo">
-				<center>
-					<h1>Licao Nao Possui Perguntas!!!</h1>
-				</center>
-				</div>
-			</div>
-			<center>
-				<div class="barra-responder tela-opcoes-1">	
-					<div class="btn-group btn-group-justified fixer-bottom">
-						<div class="btn-group">
-							<button  type="button"  onclick="window.location='/principal/'" class="btn btn-primary">
-								Voltar Inicio
-							</button>
-						</div>
+{%  block barra-inferior %}
+	<div class="barra-responder tela-opcoes-1">
+				<div class="btn-group btn-group-justified fixer-bottom">
+					<div class="btn-group">
+						<button  type="button"  onclick="window.location='/principal/'" class="btn btn-info">
+							Voltar Inicio
+						</button>
 					</div>
 				</div>
-			</center>
-		</div>
 	</div>
 {% endblock %}
