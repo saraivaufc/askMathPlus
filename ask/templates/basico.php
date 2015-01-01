4737a7ea7702582c553b67ad2ef50a0fc4dda70d
saraivaufc <saraiva.ufc@gmail.com>
2015-01-01 08:46:52 -0300

    update css

--- ask/templates/basico.php
+++ ask/templates/basico.php
@@ -100,7 +100,7 @@
 	{% block rodape_all %}
 	<footer id="rodape">
 		{% block rodape %}
-			<center id="creditos">AskMath | Copyright &copy; 2014-2015 - Programa De Educação Tutorial - Tecnologia Da Informação - PETTIQX</center>
+			<center id="creditos">AskMath | Copyright &copy; 2015 - Programa De Educação Tutorial - Tecnologia Da Informação - PETTIQX</center>
 		{% endblock %}
 	</footer>
 	{% endblock %}
