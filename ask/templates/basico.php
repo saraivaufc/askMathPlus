121cbbed95fff8d24fe39223ba380f8d177eefa5
saraivaufc <saraiva.ufc@gmail.com>
2015-01-27 11:47:20 -0300

    antes de formatar o pc

--- ask/templates/basico.php
+++ ask/templates/basico.php
@@ -8,7 +8,7 @@
 
 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<meta name="description" content="AskMath, simples, intuitivo e poderoso sistema de perguntas e respostas criado para a Universidade Federal do Ceará.">
+			<meta name="description" content="">
 			<meta name="keywords" content="Universidade, Sistema, Educação, Estudantes, Perguntas e Respostas">
 			<meta name="author" content="Ciano Saraiva">
 
