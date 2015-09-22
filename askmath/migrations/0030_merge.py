0b1aef5744e252d6618a62f38c6e25e2953b838b
root <root@debian.debian>
2015-09-22 15:46:06 -0300

    Atualizei as permissões

---
+++ askmath/migrations/0030_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('askmath', '0029_auto_20150805_1651'),
+        ('askmath', '0029_auto_20150805_1728'),
+    ]
+
+    operations = [
+    ]
