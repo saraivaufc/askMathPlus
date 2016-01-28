aa65b548ac161ac2d99d13bfd68c2155a803bec2
saraivaufc <saraiva.ufc@gmail.com>
2016-01-28 16:54:57 -0300

    COncertei bugs do Design 2

--- askmath/static/askmath/js/accessibility.js
+++ askmath/static/askmath/js/accessibility.js
@@ -1,51 +1,20 @@
-$(function(){
-	if(Cookies.get("contrast")){
-		$("body").addClass("bg-dark fg-white");	
-		$("a, h1, h2, h3").addClass("link_contrast");
-		$(".btn").addClass("btn_contrast");
-		var button_contrast = $("#button-contrast");
-		button_contrast.removeClass("disable-contrast");
-		button_contrast.addClass("enable-contrast");
-		button_contrast.text("High Contrast-ON [3]");
-	}
-	var font_size  = Cookies.get("font");
-	if(font_size === undefined){
-
-	}else{
-		set_font(font_size);
-		var button_font = $("#button-font");
-		button_font.removeClass("font-minus");
-		button_font.addClass("font-plus");
-		button_font.text("Big Font-ON [4]");
+function active_constrast(){
+	$("body").addClass("bg-dark fg-white");	
 	
-	}
-});
+	var button_contrast = $("#button-contrast");
+	button_contrast.removeClass("disable-contrast");
+	button_contrast.addClass("enable-contrast");
+	button_contrast.text("High Contrast-ON [3]");
+}
 
-$("#button-contrast").click(function(){
-	$("body").toggleClass("highcontrast");
-	if($(this).hasClass("disable-contrast")){
-		$(this).removeClass("disable-contrast");
-		$(this).addClass("enable-contrast");
-		$(this).text("High Contrast-ON [3]");
-		Cookies.set("contrast", true);
-		console.log("Contrar Enable");
-	}else if($(this).hasClass("enable-contrast")){
-		$(this).removeClass("enable-contrast");
-		$(this).addClass("disable-contrast");
-		$(this).text("High Contrast-OFF [3]");
-		Cookies.remove("contrast");
-		console.log("Contrar Disable");
-	}
-});
+function disable_constrast(){
+	$("body").removeClass("bg-dark fg-white");	
 
-$("#increase-font").click(function(){
-	alter_font(2);
-});
-
-$("#decrease-font").click(function(){
-	alter_font(-2);
-});
-
+	var button_contrast = $("#button-contrast");
+	button_contrast.removeClass("enable-contrast");
+	button_contrast.addClass("disable-contrast");
+	button_contrast.text("High Contrast-OFF [3]");
+}
 
 function set_font(size){
 	$("h1, h2, h3, h4, h5, h6, p, label, a").each(function(){
@@ -64,3 +33,42 @@ function alter_font(size){
 	}
 	Cookies.set("font", font);
 }
+
+
+
+if(Cookies.get("contrast")){
+	active_constrast();
+}
+
+var font_size  = Cookies.get("font");
+if(font_size === undefined){
+
+}else{
+	set_font(font_size);
+	var button_font = $("#button-font");
+	button_font.removeClass("font-minus");
+	button_font.addClass("font-plus");
+	button_font.text("Big Font-ON [4]");
+
+}
+
+
+$("#button-contrast").click(function(){
+	if($(this).hasClass("disable-contrast")){
+		active_constrast();
+		Cookies.set("contrast", true);
+		console.log("Contrar Enable");
+	}else if($(this).hasClass("enable-contrast")){
+		disable_constrast();
+		Cookies.remove("contrast");
+		console.log("Contrar Disable");
+	}
+});
+
+$("#increase-font").click(function(){
+	alter_font(2);
+});
+
+$("#decrease-font").click(function(){
+	alter_font(-2);
+});
