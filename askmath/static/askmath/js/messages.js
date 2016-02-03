abe86945e4298cfb8ad58cd3731124d76469c370
saraivaufc <saraiva.ufc@gmail.com>
2016-02-03 05:25:50 -0300

    Deixando as Notificações mais rápidas

--- askmath/static/askmath/js/messages.js
+++ askmath/static/askmath/js/messages.js
@@ -1,14 +1,12 @@
-$(function(){
-	$("ul.messages > li.message").each(function(){
-		noty({
-		    text: $(this).text(),
-		    theme: 'relax',
-		    type: $(this).attr("value"),
-		    animation: {
-		        open: 'animated bounceInUp', // Animate.css class names
-		        close: 'animated bounceOutDown', // Animate.css class names
-		    }
-		});
+
+$("ul.messages > li.message").each(function(){
+	noty({
+	    text: $(this).text(),
+	    theme: 'relax',
+	    type: $(this).attr("value"),
+	    animation: {
+	        open: 'animated bounceInUp', // Animate.css class names
+	        close: 'animated bounceOutDown', // Animate.css class names
+	    }
 	});
-	
 });
