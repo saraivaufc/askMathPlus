63e0b60779ea56a4546a8fb6190d8cbd37d60431
saraivaufc <saraiva.ufc@gmail.com>
2016-03-10 07:48:28 -0300

    update wsgi

--- askMathPlus/wsgi.py
+++ askMathPlus/wsgi.py
@@ -7,9 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
 import os
+import time
+import traceback
+import signal
+from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "askMathPlus.settings"
+try:
+	application = get_wsgi_application()
+	print 'WSGI without exception'
+except Exception:
+	print 'handling WSGI exception'
+	# Error loading applications
+	if 'mod_wsgi' in sys.modules:
+		traceback.print_exc()
+		os.kill(os.getpid(), signal.SIGINT)
+		time.sleep(2.5)
 
-application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# import os
+
+# os.environ["DJANGO_SETTINGS_MODULE"] = "askMathPlus.settings"
+
+# application = get_wsgi_application()
