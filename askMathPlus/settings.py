32158fa6a431bfba6faea75f6bb9b0ee863aff0d
saraivaufc <saraiva.ufc@gmail.com>
2016-06-29 05:17:30 -0300

    Processo atualizado

--- askMathPlus/settings.py
+++ askMathPlus/settings.py
@@ -181,10 +181,10 @@ SOCIAL_AUTH_PIPELINE = (
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }, 'postgres': {
+    }, 'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', 
             'NAME': 'askmath',
             'USER': 'postgres',
