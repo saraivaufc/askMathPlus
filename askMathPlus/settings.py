5184ad4a8778e10a6552e1cc415cd1cdfecbd86b
saraivaufc <saraiva.ufc@gmail.com>
2016-08-04 10:00:41 -0300

    Recuperação de Senha

--- askMathPlus/settings.py
+++ askMathPlus/settings.py
@@ -35,9 +35,9 @@ PROJECT_DIR = os.path.dirname(__file__)
 SECRET_KEY = 'hb@jze2=jvslsc%p8g&u88dhn0d3=fm!)f^o-=w=yj9)woag6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['www.askmath.quixada.ufc.br']
 # ALLOWED_HOSTS = ['localhost']
@@ -170,14 +170,14 @@ DATABASES = {
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }, 'postgres': {
+    }, 'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'askmath',
         'USER': 'postgres',
         'PASSWORD': 'macacoaranha',
         'HOST': '200.129.39.113',
         'PORT': '6969',
-    }, 'default': {
+    }, 'postgres_local': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'askmath',
         'USER': 'postgres',
--- askMathPlus/urls.py
+++ askMathPlus/urls.py
@@ -6,39 +6,38 @@ from django.template import RequestContext
 from django.views.i18n import javascript_catalog
 
 js_info_dict = {
-    'packages': ('manager',),
+		'packages': ('manager',),
 }
 
 urlpatterns = patterns('',
-                       url(r'^', include('askmath.urls', namespace="askmath", app_name="askmath")),
-                       url(r'^admin/', include(admin.site.urls), name='admin'),
-                       url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-                           {'document_root': settings.MEDIA_ROOT}),
-                       url(r'^jsi18n/$', javascript_catalog, js_info_dict),
-                       )
+	 url(r'^', include('askmath.urls', namespace="askmath", app_name="askmath")),
+	 url(r'^admin/', include(admin.site.urls), name='admin'), url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	 url(r'^jsi18n/$', javascript_catalog, js_info_dict),
+	 url('^', include('django.contrib.auth.urls')),
+	 )
 
 if 'rosetta' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
-                            url(r'^rosetta/', include('rosetta.urls')),
-                            )
+		urlpatterns += patterns('',
+														url(r'^rosetta/', include('rosetta.urls')),
+														)
 
 if 'social.apps.django_app.default':
-    urlpatterns += patterns('',
-                            url('', include('social.apps.django_app.urls', namespace='social')),
-                            )
+		urlpatterns += patterns('',
+														url('', include('social.apps.django_app.urls', namespace='social')),
+														)
 
 
 # ERRORS
 
 def handler404(request):
-    response = render_to_response('askmath/index/404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+		response = render_to_response('askmath/index/404.html', {},
+																	context_instance=RequestContext(request))
+		response.status_code = 404
+		return response
 
 
 def handler500(request):
-    response = render_to_response('askmath/index/500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
+		response = render_to_response('askmath/index/500.html', {},
+																	context_instance=RequestContext(request))
+		response.status_code = 500
+		return response
