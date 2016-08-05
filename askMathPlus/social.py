4161b53e601c3b51e92cb21cb5bfc5422cf32a72
saraivaufc <saraiva.ufc@gmail.com>
2016-08-05 09:49:05 -0300

    Concertando bug de authenticação com Twitter

--- askMathPlus/social.py
+++ askMathPlus/social.py
@@ -1,18 +1,19 @@
+"""
+Twiter: saraiva.ufc@gmail.com
+"""
 #https://developers.facebook.com/apps/?action=create
 SOCIAL_AUTH_FACEBOOK_KEY = u'1803158169916591'
 SOCIAL_AUTH_FACEBOOK_SECRET =u'755ace09b20f11d71e48b6facb0869e3'
 
 """
-Twiter: askmathplus@gmail.com  password: macacoaranha
+Twiter: askmathplus@gmail.com 
 """
 #https://apps.twitter.com/app/new 
 SOCIAL_AUTH_TWITTER_KEY = u'CNm8mXbL2xASaxuNCiCWBhLYv'
 SOCIAL_AUTH_TWITTER_SECRET = u'gxtQLYAvfSEZBmOkTnVPs9i3eueqfOCEOypU3k7SNOwhd2QBKS'
 
-
-
 """
-Gmail: askmathplus@gmail.com  password: macacoaranha
+Gmail: askmathplus@gmail.com
 """
 #https://console.developers.google.com/
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = u'806153707723-mkjnvefj9egvuilipcj55ud69javujfl.apps.googleusercontent.com'
--- askmath/models/users/person.py
+++ askmath/models/users/person.py
@@ -8,150 +8,149 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 try:
-    from hashlib import md5
+	from hashlib import md5
 except:
-    from md5 import new as md5
+	from md5 import new as md5
 
 
 class PersonManager(UserManager):
-    def create_user(self, email, password=None, group='student', **extra_fields):
-        print 'create_user with group=', group
-        email = email
-        user = self.model(
-            email=email,
-        )
-        user.set_password(password)
-        user.save()
-        return user
+	def create_user(self, email, password=None, group='student', **extra_fields):
+		print 'create_user with group=', group
+		email = email
+		user = self.model(
+			email=email,
+		)
+		user.set_password(password)
+		user.save()
+		return user
 
-    def create_superuser(self, email, password, group='administrator', **extra_fields):
-        print 'create_superuser with group=', group
-        user = self.create_user(email, password, group=group)
-        user.is_superuser = True
-        user.is_moderator = True
-        user.is_staff = True
-        user.save()
-        return user
+	def create_superuser(self, email, password, group='administrator', **extra_fields):
+		print 'create_superuser with group=', group
+		user = self.create_user(email, password, group=group)
+		user.is_superuser = True
+		user.is_moderator = True
+		user.is_staff = True
+		user.save()
+		return user
 
 
 class AbstractSystemPerson(models.Model):
-    location = models.CharField(_(u"location"), max_length=75, blank=True, null=True)
-    last_seen = models.DateTimeField(_(u"last seen"), auto_now=True)
-    last_ip = models.GenericIPAddressField(_(u"last ip"), blank=True, null=True)
-    # is_superuser = models.BooleanField(_('administrator status'), default=False,blank=True )
-    is_moderator = models.BooleanField(_('moderator status'), default=False, blank=True)
-    is_staff = models.BooleanField(_(u'staff status'), default=False,
-                                   help_text=_(u'Designates whether the user can log into this admin site.'))
+	location = models.CharField(_(u"location"), max_length=75, blank=True, null=True)
+	last_seen = models.DateTimeField(_(u"last seen"), auto_now=True)
+	last_ip = models.GenericIPAddressField(_(u"last ip"), blank=True, null=True)
+	# is_superuser = models.BooleanField(_('administrator status'), default=False,blank=True )
+	is_moderator = models.BooleanField(_('moderator status'), default=False, blank=True)
+	is_staff = models.BooleanField(_(u'staff status'), default=False,
+								   help_text=_(u'Designates whether the user can log into this admin site.'))
 
-    def get_location(self):
-        return self.location
+	def get_location(self):
+		return self.location
 
-    def get_last_seen(self):
-        return self.last_seen
+	def get_last_seen(self):
+		return self.last_seen
 
-    def get_last_ip(self):
-        return self.last_ip
+	def get_last_ip(self):
+		return self.last_ip
 
-    class Meta:
-        abstract = True
+	class Meta:
+		abstract = True
 
 
 class AbstractPerson(AbstractBaseUser, PermissionsMixin, AbstractSystemPerson):
-    first_name = models.CharField(_(u"First Name "), max_length=100, blank=False, null=True,
-                                  help_text=_(u'Please enter you first name.'), )
-    last_name = models.CharField(_(u"Last Name "), max_length=100, blank=False, null=True,
-                                 help_text=_(u'Please enter you last name.'), )
-    email = models.EmailField(_(u"Email"), max_length=254, unique=True, blank=False,
-                              help_text=_(u'Please enter you email.'), )
-    is_active = models.BooleanField(_('active'), default=True,
-                                    help_text=_(
-                                        u'Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(_(u'date joined'), default=timezone.now)
-    objects = PersonManager()
+	first_name = models.CharField(_(u"First Name "), max_length=100, blank=False, null=True,
+								  help_text=_(u'Please enter you first name.'), )
+	last_name = models.CharField(_(u"Last Name "), max_length=100, blank=False, null=True,
+								 help_text=_(u'Please enter you last name.'), )
+	email = models.EmailField(_(u"Email"), max_length=254, unique=True,  null=True, blank=True, 
+								help_text=_(u'Please enter you email.'), )
+	is_active = models.BooleanField(_('active'), default=True,
+									help_text=_(u'Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
+	date_joined = models.DateTimeField(_(u'date joined'), default=timezone.now)
+	objects = PersonManager()
 
-    def get_first_name(self):
-        return self.first_name
+	def get_first_name(self):
+		return self.first_name
 
-    def get_last_name(self):
-        return self.last_name
+	def get_last_name(self):
+		return self.last_name
 
-    def get_full_name(self):
-        if self.get_first_name():
-            if self.get_last_name():
-                return self.get_first_name() + " " + self.get_last_name()
-            else:
-                return self.get_first_name()
-        else:
-            return self.get_email()
+	def get_full_name(self):
+		if self.get_first_name():
+			if self.get_last_name():
+				return self.get_first_name() + " " + self.get_last_name()
+			else:
+				return self.get_first_name()
+		else:
+			return self.get_email()
 
-    def get_short_name(self):
-        if self.get_first_name():
-            return self.get_first_name()
-        else:
-            return self.get_email()
+	def get_short_name(self):
+		if self.get_first_name():
+			return self.get_first_name()
+		else:
+			return self.get_email()
 
-    def get_email(self):
-        return self.email
+	def get_email(self):
+		return self.email
 
-    def get_date_joined(self):
-        return self.date_joined
+	def get_date_joined(self):
+		return self.date_joined
 
-    USERNAME_FIELD = 'email'
+	USERNAME_FIELD = 'email'
 
-    def __unicode__(self):
-        return self.get_full_name()
+	def __unicode__(self):
+		return self.get_full_name()
 
-    class Meta:
-        abstract = True
+	class Meta:
+		abstract = True
 
-    def change_password(self, new_password):
-        new_password_hash = md5(new_password).hexdigest()
-        self.password = new_password_hash
-        self.save()
+	def change_password(self, new_password):
+		new_password_hash = md5(new_password).hexdigest()
+		self.password = new_password_hash
+		self.save()
 
 
 class Person(AbstractPerson):
-    profile_image = models.ImageField(verbose_name=_(u"Profile Image"), help_text=_(u"Please enter you profile image."),
-                                      upload_to='documents/image/profile_image/%Y/%m/%d', null=True, blank=True,
-                                      default=None)
-    color = models.CharField(verbose_name=_(u'Color'), max_length=50, default=generate_color)
-    exists = models.BooleanField(default=True)
+	profile_image = models.ImageField(verbose_name=_(u"Profile Image"), help_text=_(u"Please enter you profile image."),
+									  upload_to='documents/image/profile_image/%Y/%m/%d', null=True, blank=True,
+									  default=None)
+	color = models.CharField(verbose_name=_(u'Color'), max_length=50, default=generate_color)
+	exists = models.BooleanField(default=True)
 
-    def save(self, group=None, *args, **kwargs):
-        super(Person, self).save(*args, **kwargs)
+	def save(self, group=None, *args, **kwargs):
+		super(Person, self).save(*args, **kwargs)
 
-        if not group and not self.groups.all():
-            group = 'student'
-        if group and not self.groups.all():
-            print 'Create user with group=', group
-            group = Group.objects.get(name=group)
-            self.groups.add(group)
+		if not group and not self.groups.all():
+			group = 'student'
+		if group and not self.groups.all():
+			print 'Create user with group=', group
+			group = Group.objects.get(name=group)
+			self.groups.add(group)
 
-    def get_person_class(self, instance, SubClass):
-        try:
-            return SubClass.objects.get(pk=instance.pk)
-        except SubClass.DoesNotExist:
-            return instance
+	def get_person_class(self, instance, SubClass):
+		try:
+			return SubClass.objects.get(pk=instance.pk)
+		except SubClass.DoesNotExist:
+			return instance
 
-    def get_profile_image(self):
-        return self.profile_image
+	def get_profile_image(self):
+		return self.profile_image
 
-    def __unicode__(self):
-        return self.get_full_name()
+	def __unicode__(self):
+		return self.get_full_name()
 
-    def delete(self):
-        self.exists = False
-        self.save()
+	def delete(self):
+		self.exists = False
+		self.save()
 
-    def restore(self):
-        self.exists = True
-        self.save()
+	def restore(self):
+		self.exists = True
+		self.save()
 
-    class Meta(AbstractPerson.Meta):
-        abstract = False
-        db_table = 'auth_user'
-        swappable = 'AUTH_USER_MODEL'
-        app_label = 'askmath'
-        ordering = ['first_name', ]
-        verbose_name = _(u'Person')
-        verbose_name_plural = _(u'Persons')
+	class Meta(AbstractPerson.Meta):
+		abstract = False
+		db_table = 'auth_user'
+		swappable = 'AUTH_USER_MODEL'
+		app_label = 'askmath'
+		ordering = ['first_name', ]
+		verbose_name = _(u'Person')
+		verbose_name_plural = _(u'Persons')
