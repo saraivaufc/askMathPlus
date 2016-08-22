4a1aa6c0e15f1093208482060cf9e0c6d9c84e77
saraivaufc <saraiva.ufc@gmail.com>
2016-08-21 21:06:11 -0300

    ada

--- manage.py
+++ manage.py
@@ -2,12 +2,13 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
+sys.path.append(sys.path[0])
+sys.path.append(sys.path[0]+"/askMathPlus")
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "askMathPlus.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "askMathPlus.settings")
 
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
 
 #find . -name '*.pyc' -delete
